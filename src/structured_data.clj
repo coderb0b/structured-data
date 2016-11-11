43dbef3bbdfc74fccb6fb38a955710fb4d69ac90
Engelbrekt <jari.silvo@cs.helsinki.fi>
2016-11-11 17:51:46 +0200

    structured-data

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [dat (+ x x)]
+    (Math/pow dat dat)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,34 +21,46 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (= (- x2 x1) (- y2 y1)) true false)))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (* (- y2 y1) (- x2 x1))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [z1 z2] point]
+    (if (and (<= x1 z1 x2) (<= y1 z2 y2)) true false)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer
+        [[d1 t1] [d2 t2]] inner]
+    (if (and (contains-point? (rectangle [x1 y1] [x2 y2])
+                 (point d1 t1)) (contains-point? (rectangle [x1 y1] [x2 y2])
+                 (point d2 t2))) true false)))
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if (> (count (get book :authors)) 1) true false))
 
 (defn add-author [book new-author]
-  :-)
+  (let [b book
+        a new-author]
+    (assoc b :authors (conj (get b :authors) a))))
 
 (defn alive? [author]
   :-)
